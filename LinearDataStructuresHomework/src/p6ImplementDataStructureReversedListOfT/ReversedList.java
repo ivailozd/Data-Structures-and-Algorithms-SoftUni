82364c98e23625e55f45d98b4a29c19abec91dbb
ivailozd <ivailozd@gmail.com>
2015-07-17 11:52:32 +0300

    reversed list implementation

--- LinearDataStructuresHomework/src/p6ImplementDataStructureReversedListOfT/ReversedList.java
+++ LinearDataStructuresHomework/src/p6ImplementDataStructureReversedListOfT/ReversedList.java
@@ -1,15 +1,79 @@
 package p6ImplementDataStructureReversedListOfT;
 
+import java.util.Arrays;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
-public class ReversedList<T> implements Iterable<T>{
+public class ReversedList<T> implements Iterable<T>, Iterator<T>{
 
+	private int size = 0;
+	private static final int DEFAULT_CAPACITY = 3;
+	private Object elements[];
+	private int current = 0;
 	
+	public ReversedList() {
+	  elements = new Object[DEFAULT_CAPACITY];
+	}
+
+	public void add(T e) {
+	  if (size == elements.length) {
+	    ensureCapacity();
+	  }  
+	    
+	  elements[size++] = e;
+	}
+	 
+
+	private void ensureCapacity() {
+	  int newSize = elements.length * 2;
+	  elements = Arrays.copyOf(elements, newSize);
+	}
+	  
+	public int count() {
+		return size;
+	}
+	
+	public int capacity() {
+		return elements.length;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public T get(int index) {
+		if (index < 0 || index >= size) {
+			throw new IndexOutOfBoundsException("Invalid index: " + index + ", Count: " + size);
+		}
+		
+		return (T) elements[size - index - 1];
+	}
+	
+	public void remove(int index) {
+		if (index < 0 || index >= size) {
+			throw new IndexOutOfBoundsException("Invalid index: " + index + ", Count: " + size);
+		}
+		
+		index = size - index - 1;
+		
+		System.arraycopy(elements, index + 1, elements, index, elements.length - (index + 1));
+		elements[size - 1] = null;
+		size--;
+	}
 	
 	@Override
 	public Iterator<T> iterator() {
-		// TODO Auto-generated method stub
-		return null;
+		return this;
 	}
 
+	@Override
+	public boolean hasNext() {
+		return current < size;
+	}
+
+	@Override
+	public T next() {
+		if (! hasNext()) {
+			throw new NoSuchElementException();
+		}
+		
+		return get(current++);
+	}
 }
